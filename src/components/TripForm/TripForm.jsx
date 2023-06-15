d90690310c4247f6d0a5e1d8fc8a2de035efb89d
Ariadna <ariadnadcm@gmail.com>
2023-06-15 16:49:13 +0100

    styled and added real fields in tripform

--- src/components/TripForm/TripForm.jsx
+++ src/components/TripForm/TripForm.jsx
@@ -1,26 +1,210 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Card, CardContent, CardActions, Typography, Box  } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardActions,
+  Typography,
+  Box,
+  Grid,
+  Paper,
+  TextField,
+  InputLabel,
+  CardHeader,
+} from "@mui/material";
+import { styled } from "@mui/material";
 
-import React from 'react'
+import React from "react";
 
 function TripForm() {
-
-    const [date, setDate] = useState("");
-    const [departureTime, setDepartureTime] = useState("");
-    const [availableSeats, setAvailableSeats] = useState("");
-    const [vehicleType, setVehicleType] = useState("");
-    const [favoriteGenre, setFavoriteGenre] = useState("");
-    const [lenguaje, setLenguaje] = useState("");
-    const [petAccepted, setpetAccepted] = useState("");
-    const [maximumBaggage, setMaximumBaggage] = useState("");
+  const [date, setDate] = useState("");
+  const [departureTime, setDepartureTime] = useState("");
+  const [availableSeats, setAvailableSeats] = useState("");
+  const [vehicleType, setVehicleType] = useState("");
+  const [favoriteGenre, setFavoriteGenre] = useState("");
+  const [lenguaje, setLenguaje] = useState("");
+  const [petAccepted, setpetAccepted] = useState("");
+  const [maximumBaggage, setMaximumBaggage] = useState("");
 
 
   return (
-    <div>
-      <h1>Create your trip here</h1>
-    </div>
+    <Card
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        paddingTop:"70px",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw"
+      }}
+    >      
+      <CardHeader title="Create Trip" />
+      <CardContent sx={{display: 'flex', justifyContent: 'center', alignContent: 'center'}}>
+        <Box sx={{ width: "50%" }}>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
+            <Grid
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+              }}
+              item
+              xs={6}
+            >
+              <TextField
+              fullWidth={true}
+                sx={{ width: "50%" }}
+                id="outlined-basic"
+                label="Date"
+                variant="outlined"
+              />
+            </Grid>
+
+            <Grid
+              item
+              xs={6}
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+              }}
+            >
+              <TextField
+                sx={{ width: "50%" }}
+                id="outlined-basic"
+                label="Lenguaje"
+                variant="outlined"
+              />
+            </Grid>
+
+            <Grid
+              item
+              xs={6}
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+              }}
+            >
+              <TextField
+                sx={{ width: "50%" }}
+                id="outlined-basic"
+                label="Departure time"
+                variant="outlined"
+              />
+            </Grid>
+
+            <Grid
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+              }}
+              item
+              xs={6}
+            >
+              <TextField
+                sx={{ width: "50%" }}
+                id="outlined-basic"
+                label="Driving skill"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+              }}
+              item
+              xs={6}
+            >
+              <TextField
+                sx={{ width: "50%" }}
+                id="outlined-basic"
+                label="Avaible seats"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+              }}
+              item
+              xs={6}
+            >
+              <TextField
+                sx={{ width: "50%" }}
+                id="outlined-basic"
+                label="Pets accepted"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+              }}
+              item
+              xs={6}
+            >
+              <TextField
+                sx={{ width: "50%" }}
+                id="outlined-basic"
+                label="Vehicle type"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+              }}
+              item
+              xs={6}
+            >
+              <TextField
+                sx={{ width: "50%" }}
+                id="outlined-basic"
+                label="Maximum baggage"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+              }}
+              item
+              xs={6}
+            >
+              <TextField
+                sx={{ width: "50%" }}
+                id="outlined-basic"
+                label="Favorite genre"
+                variant="outlined"
+              />
+            </Grid>
+          </Grid>
+        </Box>
+      </CardContent>
+          <CardActions>
+            <Button variant="contained" color="error" sx={{ width: "100%" }}>
+              Crear viaje{" "}
+            </Button>
+          </CardActions>
+    </Card>
   );
 }
 
-export default TripForm
+export default TripForm;
