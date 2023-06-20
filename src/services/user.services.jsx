02a3d808b27b84ad22e77a88472af3ccca447755
OscarQGlez <osquevgon@gmail.com>
2023-06-20 10:12:34 +0100

    General: modificacion en el  servicio user

--- src/services/user.services.jsx
+++ src/services/user.services.jsx
@@ -1,13 +1,10 @@
 import { api } from "./api";
-export const addtrip = async ( date,departure,available_seats,vehicle_type,favorite_genre,
-    lenguaje,driving_skill,pets_accepted,maximun_baggage) => {
+export const adduser = async (id) => {
     try {
-        const { data } = await api.post('/trip', {date,departure,available_seats,vehicle_type,favorite_genre,
-            lenguaje,driving_skill,pets_accepted,maximun_baggage}, { 
-                headers: {
-                token: localStorage.getItem('token')
-              }
-            })
+        const { data } = await api.post('/trip', {}, { headers: { token: localStorage.getItem('token') } } )
+
+        return data
+            
 
     } catch (error) {
         res.status(500).send(error.message)
